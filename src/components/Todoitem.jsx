de6cde1b946754948a40ca37b5fd1bd57f774c51
Hemangi <hemangisolanki04@gmail.com>
2024-06-20 18:04:42 +0530

    add new change

--- src/components/Todoitem.jsx
+++ src/components/Todoitem.jsx
@@ -1,12 +1,13 @@
+import { MdDelete } from "react-icons/md";
 function Todoitem({todoName, todoDate,onDeleteClick}) {
 
   return   <div className="items-container ">
   <div className="kg-row row">
     <div className="col-6">{todoName} </div>
     <div className="col-4">{todoDate}</div>
-    <div className="col-2"><button type="button" className="btn btn-danger kg-button" onClick={() => onDeleteClick(todoName)}>
-      
-      Delete</button>
+    <div className="col-2"><button type="button" className="btn btn-danger kg-button button" onClick={() => onDeleteClick(todoName)}>
+    <MdDelete  className="icon"/>
+      </button>
     </div>
 
   </div>
